bcc53514e0386aeae68e7e09d5a8609a82c8dab2
Dilshod <avlomberdioglid@gmail.com>
2023-06-16 17:16:40 +0500

    xonani band qilishda oldin band qilinga vaqtga yana band qilish imkoniyati olib tashlandi

--- routes/bookings.js
+++ routes/bookings.js
@@ -8,9 +8,14 @@ router.post('/:Id/book', async (req, res) => {
     if (error)
         res.status(400).send(error.details[0].message);
 
-    const room = await Room.findById(req.params.Id);
+    let room = await Room.findById(req.params.Id);
     if (!room)
         return res.status(404).send("Berilgan Id ga mos xona topilmadi!");
+
+    if(room.start <= req.body.start && room.end >= req.body.end)
+    	return res.status(410).send({
+  			"error": "uzr, siz tanlagan vaqtda xona band"
+		});
 
 
     const booking = new Booking({
@@ -27,14 +32,48 @@ router.post('/:Id/book', async (req, res) => {
     });
 });
 
+
+// router.post('/:Id/book', async (req, res) => {
+//     const roomId = req.params.Id;
+//     const startTime = new Date(req.body.start);
+//     const endTime = new Date(req.body.end);
+//     let room = await Room.findById(req.params.Id);
+//     if (!room)
+//         return res.status(404).send("Berilgan Id ga mos xona topilmadi!");
+
+//     const booking = await Booking.find({
+//         roomId,
+//         $and: [
+//             { start: { $gte: startTime, $lt: endTime } },
+//             { end: { $gt: startTime, $lte: endTime } }
+//         ]
+//     });
+
+//     if (booking) {
+//         res.status(410).send({ error: 'uzr, siz tanlagan vaqtda xona band' });
+//     } else {
+//         // time slot is available
+//         const booking = new Booking({
+//             room: room._id,
+//             resident: req.body.resident,
+//             start: startTime,
+//             end: endTime
+//         });
+
+//         await booking.save();
+
+//         res.send({ message: 'xona muvaffaqiyatli band qilindi' });
+//     }
+// });
+
+
 router.get('/:Id/availability', async (req, res) => {
     const roomAvailability = await Booking.find({
             room: req.params.Id,
-            end: { $gt: new Date() }
+            // end: { $gte: new Date() }
 
         })
-        .select({ _id: 0, start: 1, end: 1 });
-    const startDate = roomAvailability.start;
+        .select({ _id: 0, start: 1, end: 1 })
 
     return res.status(200).send(roomAvailability);
 });
