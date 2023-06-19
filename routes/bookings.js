515c53bb020a3ac9acc03a8daf32b6eafcbd68c8
Dilshod <avlomberdioglid@gmail.com>
2023-06-19 22:51:43 +0500

    xonani book qilishda oldin book qilingan vaqtga yana qaytada book qilish fix qilindi

--- routes/bookings.js
+++ routes/bookings.js
@@ -4,6 +4,8 @@ const { Room } = require('../models/room');
 const { validateBooking, Booking } = require('../models/booking');
 
 router.post('/:Id/book', async (req, res) => {
+    const startTime = new Date(req.body.start);
+    const endTime = new Date(req.body.end);
     const { error } = validateBooking(req.body);
     if (error)
         res.status(400).send(error.details[0].message);
@@ -12,60 +14,31 @@ router.post('/:Id/book', async (req, res) => {
     if (!room)
         return res.status(404).send("Berilgan Id ga mos xona topilmadi!");
 
-    if(room.start <= req.body.start && room.end >= req.body.end)
-    	return res.status(410).send({
-  			"error": "uzr, siz tanlagan vaqtda xona band"
-		});
 
-
-    const booking = new Booking({
-        room: room._id,
-        resident: req.body.resident,
-        start: req.body.start,
-        end: req.body.end
+    const bookedRoom = await Booking.find({
+        room: req.params.Id
     });
+    
 
-    await booking.save();
+    if(bookedRoom.length !== 0 && startTime>=bookedRoom[0].start && startTime<=bookedRoom[0].end){
+        res.status(410).send({error: 'uzr, siz tanlagan vaqtda xona band'});
+    } else if(startTime < new Date() && endTime <= startTime){
+        res.status(410).send({error: 'uzr, siz tanlagan vaqt oralig\'i noto\'ri!'});
+    } else{
+        const booking = new Booking({
+            room: room._id,
+            resident: req.body.resident,
+            start: req.body.start,
+            end: req.body.end
+        });
 
-    res.send({
-        message: "xona muvaffaqiyatli band qilindi"
-    });
+            await booking.save();
+    
+            res.send({
+                message: "xona muvaffaqiyatli band qilindi"
+        });
+    }
 });
-
-
-// router.post('/:Id/book', async (req, res) => {
-//     const roomId = req.params.Id;
-//     const startTime = new Date(req.body.start);
-//     const endTime = new Date(req.body.end);
-//     let room = await Room.findById(req.params.Id);
-//     if (!room)
-//         return res.status(404).send("Berilgan Id ga mos xona topilmadi!");
-
-//     const booking = await Booking.find({
-//         roomId,
-//         $and: [
-//             { start: { $gte: startTime, $lt: endTime } },
-//             { end: { $gt: startTime, $lte: endTime } }
-//         ]
-//     });
-
-//     if (booking) {
-//         res.status(410).send({ error: 'uzr, siz tanlagan vaqtda xona band' });
-//     } else {
-//         // time slot is available
-//         const booking = new Booking({
-//             room: room._id,
-//             resident: req.body.resident,
-//             start: startTime,
-//             end: endTime
-//         });
-
-//         await booking.save();
-
-//         res.send({ message: 'xona muvaffaqiyatli band qilindi' });
-//     }
-// });
-
 
 router.get('/:Id/availability', async (req, res) => {
     const roomAvailability = await Booking.find({
